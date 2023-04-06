88f933ff876f6724320dda4d6d30892fee53dfeb
Wonhee Kim <66175249+wonhee418@users.noreply.github.com>
2023-04-06 09:30:31 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,9 +1,7 @@
 function solution(n) {
     let result = 0
-    for(let i = 0; i <= n; i++){
-        if(i % 2 === 0 ){
-            result = result + i 
-        }
+    for(let i = 2; i <= n; i += 2){
+        result += i 
     }
     return result
 }
