10f4ffd26313bd0498295fc4861c1d2159724368
Wonhee Kim <66175249+wonhee418@users.noreply.github.com>
2023-04-11 12:57:18 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,10 +1,4 @@
 function solution(my_string, n) {
-    let result = '';
     const str = my_string.split('')
-    str.map((s)=>{
-        for(let i = 0; i < n; i++){
-            result += s
-        }    
-    })
-    return result;
+    return str.reduce((arr,cur)=> arr+cur.repeat(n),"")
 }
